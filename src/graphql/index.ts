bdf086c203ae7b2161807b1fab92557b01efd78e
Mohit Soni <developer_mohitsoni@outlook.com>
2024-10-25 19:46:51 +0530

    Basically this code creating apolloServer which aggregate all queries and mutations and resolvers from different files

---
+++ src/graphql/index.ts
@@ -0,0 +1,33 @@
+import { ApolloServer } from "@apollo/server";
+import { User } from "./user";
+import { gql } from "apollo-server-express";
+
+export const createApolloGraphqlServer = async () => {
+  const gqlServer = new ApolloServer({
+    typeDefs: gql`
+      #graphql
+
+      type Query {
+        ${User.queries}
+      }
+      type Mutation {
+        ${User.mutations}
+      }
+    `, // Schema
+
+    resolvers: {
+      Query: {
+        ...User.resolvers.queries,
+      },
+      Mutation: {
+        ...User.resolvers.mutations,
+      },
+    },
+  });
+
+  // Start the gql Server
+
+  await gqlServer.start();
+
+  return gqlServer;
+};
