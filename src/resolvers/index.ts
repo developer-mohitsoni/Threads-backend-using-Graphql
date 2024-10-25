87700ef2b1109335ae3f776a3960de6e62f13f5e
Mohit Soni <developer_mohitsoni@outlook.com>
2024-10-25 18:33:25 +0530

    Add createUser mutation to resolvers

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { prismaClient } from "../lib/db";
 
 const resolvers = {
   Query: {
@@ -7,6 +8,33 @@ const resolvers = {
     },
     say: (_: any, { name }: { name: string }) => {
       return `Hello, ${name}!`;
+    },
+  },
+  Mutation: {
+    createUser: async (
+      _: any,
+      {
+        firstName,
+        lastName,
+        email,
+        password,
+      }: {
+        firstName: string;
+        lastName: string;
+        email: string;
+        password: string;
+      }
+    ) => {
+      await prismaClient.user.create({
+        data: {
+          firstName,
+          lastName,
+          email,
+          password,
+          salt: "random_salt",
+        },
+      });
+      return true;
     },
   },
 };
