95ab10ef731ba16485d22bfaf1ca101304512a16
Mohit Soni <developer_mohitsoni@outlook.com>
2024-10-25 19:38:32 +0530

    Setup Apollo Server basically we create Microservice Architecture pattern follow

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,34 @@
-import { app, startApolloServer } from "./app";
+import express, { Application, Request, Response } from "express";
+import { expressMiddleware } from "@apollo/server/express4";
+import { createApolloGraphqlServer } from "./graphql";
+
+import cors from "cors";
+import bodyParser from "body-parser";
+
 import "dotenv/config";
 
-const PORT = Number(process.env.PORT) || 8000;
+const init = async () => {
+  const app: Application = express();
 
-const startServer = async () => {
-  await startApolloServer();
+  app.use(bodyParser.json());
+  app.use(express.json());
+  app.use(cors());
+
+  const PORT = Number(process.env.PORT) || 8000;
+
+  app.get("/", (req: Request, res: Response) => {
+    res.json({
+      message: "Hello Graphql",
+    });
+  });
+
+  const gqlServer = await createApolloGraphqlServer();
+
+  app.use("/graphql", expressMiddleware(gqlServer));
 
   app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}/graphql`);
   });
 };
 
-startServer();
+init();
